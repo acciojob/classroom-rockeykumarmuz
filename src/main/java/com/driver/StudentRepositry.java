bfd44be0e82e79fcd89e0464a217fb8da0db0a6b
Rockey Kumar <rockeykumarmuz@gmail.com>
2023-07-12 21:38:22 +0530

    completed assignment

---
+++ src/main/java/com/driver/StudentRepositry.java
@@ -0,0 +1,85 @@
+package com.driver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class StudentRepositry {
+
+    HashMap<String, Student> studentDb= new HashMap<>();
+    HashMap<String, Teacher> teacherDb= new HashMap<>();
+    HashMap<String, List<String>> studentTeacherDb= new HashMap<>();
+
+    public String addStudent(Student student) {
+
+        studentDb.put(student.getName(), student);
+        return "added successfully";
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teacherDb.put(teacher.getName(), teacher);
+    }
+
+    public void addStudentTeacherPair(String student, String teacher) {
+    // put mapping is only ofr existing case that's why we are checking in student and teacher db
+        if (studentDb.containsKey(student) && teacherDb.containsKey((teacher))) {
+
+            if (studentTeacherDb.containsKey(teacher)) {
+                List<String> list = studentTeacherDb.get(teacher);
+                list.add(student);
+                studentTeacherDb.put(teacher, list);
+            } else {
+                List<String> al = new ArrayList<>();
+                al.add(student);
+                studentTeacherDb.put(teacher, al);
+            }
+        }
+    }
+
+    public Student getStudentByName(String name) {
+        if(studentDb.containsKey(name)) {
+            return studentDb.get(name);
+        } else {
+            return null;
+        }
+    }
+
+    public Teacher getTeacherByName(String name) {
+        if(teacherDb.containsKey(name)) {
+            return teacherDb.get(name);
+        } else {
+            return null;
+        }
+    }
+
+    public List<String> getStudentsByTeacherName(String teacher) {
+        if(studentTeacherDb.containsKey(teacher)) {
+            return studentTeacherDb.get(teacher);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    public List<String> getAllStudents() {
+        List<String> ans = new ArrayList<>();
+        for(String k: studentDb.keySet()) {
+            ans.add(k);
+        }
+        return ans;
+    }
+
+    public void deleteTeacherByName(String teacher) {
+        teacherDb.remove(teacher);
+        studentTeacherDb.remove(teacher);
+    }
+
+    public void deleteAllTeachers() {
+        for(String k: teacherDb.keySet()) {
+            teacherDb.remove(k);
+        }
+
+        for(String k: studentTeacherDb.keySet()) {
+            studentTeacherDb.remove(k);
+        }
+    }
+}
---
+++ src/main/java/com/driver/StudentService.java
@@ -0,0 +1,46 @@
+package com.driver;
+
+import java.util.List;
+
+public class StudentService {
+
+    StudentRepositry studentRepositry= new StudentRepositry();
+
+    public String addStudent(Student student) {
+        String ans = studentRepositry.addStudent(student);
+        return ans;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        studentRepositry.addTeacher(teacher);
+    }
+
+    public void addStudentTeacherPair(String student, String teacher) {
+        studentRepositry.addStudentTeacherPair(student, teacher);
+    }
+
+    public Student getStudentByName(String name) {
+        return studentRepositry.getStudentByName(name);
+    }
+
+    public Teacher getTeacherByName(String name) {
+        return studentRepositry.getTeacherByName(name);
+    }
+
+    public List<String> getStudentsByTeacherName(String teacher) {
+
+        return studentRepositry.getStudentsByTeacherName(teacher);
+    }
+
+    public List<String> getAllStudents() {
+        return studentRepositry.getAllStudents();
+    }
+
+    public void deleteTeacherByName(String teacher) {
+        studentRepositry.deleteTeacherByName(teacher);
+    }
+
+    public void deleteAllTeachers() {
+        studentRepositry.deleteAllTeachers();
+    }
+}
